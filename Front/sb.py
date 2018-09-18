3141049682631143aa5891054c04d44414039ec4
Hiroki.T <hirodora@me.com>
2018-09-18 21:09:44 +0900

    add: 現在のパターンが更新されるように (実際にRaspberry Pi側から光らせた信号が届いてから更新するようにしたほうがいいかもしれない)

--- Front/sb.py
+++ Front/sb.py
@@ -6,17 +6,21 @@ CURRENT_DIRNAME = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, static_folder='./templates/assets')
 pattern_list = ['blink', 'hoge', 'fuga', 'nyan', 'kiee']
 
+now_pattern = ' '
+
 @app.route('/')
 def index():
-    now_pattern = 'TEST_NOW_PATTERN'
     return render_template('index.html', now_pattern=now_pattern, pattern_list=pattern_list, pattern_length_div_by_3_int=int(len(pattern_list)/3))
 
 
 @app.route('/set', methods=['POST'])
 def set_pattern():
+    global now_pattern
     # TODO: 処理を書く
     # debug
-    print('GET:', request.form)
+    print('GET:', request.form['pattern'])
+    # TODO: 余裕があれば、実際にRaspberry Pi側から完了信号が届いてからnow_patternを更新
+    now_pattern = request.form['pattern']
     return redirect('/')
 
 def main():
