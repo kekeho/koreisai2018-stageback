2b9d2f5742b21650afadb023084a9821d2beda3e
Hiroki.T <hirodora@me.com>
2018-10-19 07:34:43 +0900

    add: monocolor

--- Front/sb.py
+++ Front/sb.py
@@ -7,7 +7,7 @@ CURRENT_DIRNAME = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__, static_folder='./templates/assets')
 pattern_list = ['blink', 'hoge', 'fuga', 'nyan', 'kiee']
-color_list = ['white', 'red', 'green', 'blue', 'yellow', 'hotpink', 'aqua', 'darkorange', 'lime',]
+color_list = ['white', 'red', 'green', 'blue', 'yellow', 'hotpink', 'aqua', 'darkorange', 'lime']
 
 now_color_button = 'white'
 now_pattern = ' '
@@ -26,24 +26,27 @@ def set_pattern():
     global now_pattern
     global now_color_button
     global led
-    request_pattern = request.form['pattern']
+    request_pattern = request.form.getlist('pattern')
     request_color = request.form.getlist('color')
-    if request_color:
-        now_color_button = request_color
-        
+    print('REQ COL:', request_color)
 
-        
-    if request_pattern == 'clear':
-        # LEDすべてOFF
-        led.off()
-    elif request_pattern == 'MONOCOLOR':
+    if request_pattern:
+        if request_pattern[0] == 'clear':
+            # LEDすべてOFF
+            led.off()
+        elif request_pattern[0] == 'MONOCOLOR':
+            now_pattern = request_pattern[0]
+            hexcolor = colors.cnames[now_color_button][1:].lower()
+            led.color(hexcolor)
+            print('SET PTN')
+
+    elif request_color:
+        now_color_button = request_color[0]
         hexcolor = colors.cnames[now_color_button][1:].lower()
         led.color(hexcolor)
+        print('SET COL')
 
-
-    print('GET:', request.form['pattern'])  # debug
     # TODO: 余裕があれば、実際にRaspberry Pi側から完了信号が届いてからnow_patternを更新
-    now_pattern = request.form['pattern']
     return redirect('/')
 
 
