8fa6187021a15de4c12caacc078caf25edcb6a03
Hiroki.T <hirodora@me.com>
2018-10-19 06:47:57 +0900

    add: just white initial commit

--- Front/lib.py
+++ Front/lib.py
@@ -8,7 +8,7 @@ sys.path.append(CURRENT_DIRNAME + '/../lib/python')
 from neopixel import *
 
 # LED strip configuration:
-LED_COUNT = 32      # Number of LED pixels.
+LED_COUNT = 2000      # Number of LED pixels.
 LED_PIN = 12      # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -81,23 +81,16 @@ class LEDObject():
                 self.now_color.append(int_to_hexcolor(color, 'lib'))
         self.strip.show()
         self.now_status = 'on'
-<<<<<<< HEAD
         print(self.now_color) #debug
-=======
->>>>>>> feature/flask
 
     def animation(self, pattern: str):
         """set animation
         """
-<<<<<<< HEAD
         pass
         # if pattern is 'blink':
         #     led.off
 
         # self.now_status = 'on'
-=======
-        self.now_status = 'on'
->>>>>>> feature/flask
 
 
 def main():
